0c50a2263d6daf569214901f1164d7c1258d1dd0
subhaDTECH <76237973+subhaDTECH@users.noreply.github.com>
2022-01-09 14:57:14 +0530

    project

---
+++ .history/index_20211209093218.js
@@ -0,0 +1,21 @@
+const students=[
+    {
+    name:"suva duley",
+    email:"suva@gmail.com"
+   },
+   {
+       name:"ram",
+       email:"suva@gmail.com"
+   }
+   {
+    name:"ram1",
+    email:"rama@gmail.com"
+}
+
+]
+
+const a=students.filter((student)=>{
+     
+    return student.email!=="suva@gmail.com";
+})
+console.log(a);
